da1f858a33ce5ca744511cb33cdeeade6a84a9ba
JuanBautista-hub <gomezperezjuanbautista@gmail.com>
2022-08-06 10:09:35 -0500

    Add project files.

---
+++ ClassMethods/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ClassMethods {
+
+    class Program {
+
+        static void Main(string[] args) { 
+        School school = new School();
+            school.name = "Jhon";
+            school.direction = "Villahermosa, Tab";
+            school.dateFundation = DateTime.Now;
+            Console.WriteLine(school.name);
+            Console.WriteLine("Stamping ....");
+            school.Stamp();
+
+        }
+    }
+
+    class School {
+
+       public string name;
+       public string direction;
+       public DateTime dateFundation;
+
+        public void Stamp() { 
+            //all
+            Console.Beep(10000,3000);
+        }
+    
+    }
+}
